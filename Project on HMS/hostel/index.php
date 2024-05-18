38fe57c6d81ae738fd02eebe037261c9c612b2da
gauravchauhan25 <gkumarc121@gmail.com>
2024-05-18 13:15:31 +0530

    Add files via upload

---
+++ Project on HMS/hostel/index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Home-HMS</title>
+
+      <link rel="stylesheet" href="css/home.css">
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+      <link rel="preconnect" href="https://fonts.googleapis.com">
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+      <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
+      <script src="../assets/js/color-modes.js"></script>
+      <script src="../assets/dist/js/bootstrap.bundle.min.js"></script>
+
+      <link rel="canonical" href="https://getbootstrap.com/docs/5.3/examples/carousel/">
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@docsearch/css@3">
+      <link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet">
+
+</head>
+
+<body>
+      <header>
+            <h2 class="logo">Maharaja Surajmal Institute</h2>
+            <nav class="navigation">
+                  <a href="index.php">Home</a>
+                  <a href="about-us.php" target="_blank">About</a>
+                  <a href="contact-us.php" target="_blank">Contact</a>
+
+                  <button class="btnLogin-popup" href="signin.php">Log In</button>
+                  <button class="btnLogin-popup" href="hostel.php">Sign Up</button>
+                  <!-- <button><a class="btnLogin-popup" href="index.php">Get Started</a></button> -->
+            </nav>
+      </header>
+
+      <!-- Main -->
+      <div class="background"></div>
+
+      <div class="content">
+            <h1>Hostel Management System</h1>
+      </div>
+
+      <div>
+            <center>
+                  <p><a class="btn btn-lg btn-primary" href="index.php">Get Started</a></p>
+            </center>
+      </div>
+
+</body>
+
+</html>
